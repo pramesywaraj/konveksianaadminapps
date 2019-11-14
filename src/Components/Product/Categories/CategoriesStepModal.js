d69ae92a17afec99fc9d73bdec5bfc8b77cd0735
Pramesywara Jembar P <pramesywara_jp@apps.ipb.ac.id>
2019-11-14 10:46:16 +0700

    add customized dialog component

--- src/Components/Product/Categories/CategoriesStepModal.js
+++ src/Components/Product/Categories/CategoriesStepModal.js
@@ -1,44 +1,59 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Dialog from '@material-ui/core/Dialog';
+import React, { useState, useEffect } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Dialog from "@material-ui/core/Dialog";
+import MuiDialogTitle from "@material-ui/core/DialogTitle";
+import MuiDialogContent from "@material-ui/core/DialogContent";
+import IconButton from "@material-ui/core/IconButton";
+import CloseIcon from "@material-ui/icons/Close";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
 
 const styles = makeStyles(theme => ({
-    // modal: {
-    //     display: "flex",
-    //     alignItems: "center",
-    //     justifyContent: "center",
-    // },
+    root: {
+        margin: 0,
+        padding: theme.spacing(2)
+    },
+    closeButton: {
+        position: "absolute",
+        right: theme.spacing(1),
+        top: theme.spacing(1),
+        color: theme.palette.grey[500]
+    },
+    paper: {
+        height: '100%'
+    }
 
-    // paper: {
-    //     backgroundColor: theme.palette.background.paper,
-    //     borderRadius: '15px',
-    //     boxShadow: theme.shadows[5],
-    //     padding: theme.spacing(5, 10),
-    //     width: '50%'
-    // },
-
-    // root: {
-    //     top: '121px',
-    //     left: 0,
-    //     right: 0,
-    //     bottom: 0,
-    //     zIndex: '-1',
-    //     position: 'fixed',
-    //     touchAction: 'none',
-    //     backgroundColor: 'rgba(0, 0, 0, 0.5)',
-    // }
 }));
 
 export default function CategoriesStepModal(props) {
     const classes = styles();
     return (
         <Dialog
+            scroll='paper'
+            classes={
+                {
+                    paper: classes.paper
+                }
+            }
+            fullWidth={true}
             open={true}
-            container={() => document.getElementById('for-step-modal')}
-            style={{position: 'absolute'}}
-            BackdropProps={{ style: { position: 'absolute' } }}
+            container={() => document.getElementById("for-step-modal")}
+            style={{ position: "absolute" }}
+            BackdropProps={{ style: { position: "absolute" } }}
+            aria-labelledby="categories-step-dialog"
         >
-           Hahahah
+            <MuiDialogTitle disableTypography className={classes.root}>
+                <IconButton
+                    aria-label="close"
+                    className={classes.closeButton}
+                >
+                    <CloseIcon />
+                </IconButton>
+            </MuiDialogTitle>
+            <MuiDialogContent>
+                <Typography variant="h6">Ini Isinya</Typography>
+
+            </MuiDialogContent>
         </Dialog>
-    )
+    );
 }
